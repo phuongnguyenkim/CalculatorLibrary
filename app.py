5393c7f01eea2aeb163860cc51fbff67f31c892a
nkphuong <nguyenkimphuongit@gmail.com>
2018-11-27 16:23:21 +0700

    #update2
    Out of range at line 51. Checking

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
+import googleapiclient
+import sys
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = [
@@ -14,33 +16,81 @@ SCOPES = [
     'https://www.googleapis.com/auth/drive.readonly'
     ]
 
-def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('drive', 'v3', http=creds.authorize(Http()))
+MIME_TYPE = {
+    "docs": "application/vnd.google-apps.document",
+    "sheets": "application/vnd.google-apps.spreadsheet",
+    "slides": "application/vnd.google-apps.presentation",
+    "forms": "application/vnd.google-apps.form",
+    "folder": "application/vnd.google-apps.folder"
+}
 
-    # Call the Drive v3 API
-    results = service.files().list(
-        fields="nextPageToken, files(id)",
-        q="(mimeType = 'application/vnd.google-apps.spreadsheet' and sharedWithMe = true)"
-        ).execute()
-    items = results.get('files', [])
-    for i in range(len(items)):
-        dictID = items[i]
+dict_FileCreate = {}
+dict_FolderCreate = {}
+
+def verify_FolderID(idFolder):
+    if (len(idFolder) <= 33):
         try:
-            service.files().copy(fileId=dictID['id'],body="(parents = '1k872-nrq8jjICyGyYTNeWbhHBBFaU6Yn')").execute()
-        except:
-            continue
+            _folderId = service.files().get(fileId=idFolder, fields='id').execute()
+        except Exception:
+            print("Fail to get Folder ID")
+        else:
+            listchild_FolderID(str(_folderId.get('id')))
+    else:
+        sys.exit("Wrong form Folder ID")
 
+def listchild_FolderID(idFolder):
+    key = 0
+    resultsId = service.files().list(
+        q="mimeType='application/vnd.google-apps.folder' and '{}' in parents".format(idFolder),
+        fields="files(id, name, parents, mimeType)").execute()
+    if resultsId:
+        print(resultsId)
+        list_temp = resultsId['files']
+        while list_temp:
+            for i in range(len(list_temp)):
+                dict_temp = list_temp[i]
+                dict_FolderCreate[key] = {"id": dict_temp['id'],
+                                        "name": dict_temp['name'],
+                                        "mimeType": dict_temp['mimeType'],
+                                        "parents": dict_temp['parents']}
+                print("Folder: {}".format(dict_FolderCreate[key]['name']))
+                list_FileID(dict_FolderCreate[key]['id'])
+                key += 1
+                list_temp.remove(list_temp[0])
 
-if __name__ == '__main__':
-    main()
+#Support Google offices extensions
+def list_FileID(idFolder):
+    key = 0
+    resultsId = service.files().list(
+        q="'{}' in parents \
+        and (mimeType contains '/vnd.google-apps.document' \
+        or mimeType contains '/vnd.google-apps.spreadsheet' \
+        or mimeType contains '/vnd.google-apps.presentation' \
+        or mimeType contains '/vnd.google-apps.form')".format(idFolder),
+        pageSize=10,
+        fields="files(id, name, mimeType, parents)").execute()
+    if resultsId:
+        list_temp = resultsId['files']
+        for i in range(len(list_temp)):
+            dict_temp = list_temp[i]
+            dict_FileCreate[key] = {"id": dict_temp['id'],
+                                    "name": dict_temp['name'],
+                                    "mimeType": dict_temp['mimeType'],
+                                    "parents": dict_temp['parents']}
+            print("Name: {}".format(dict_FileCreate[key]['name']))
+            key += 1
+            
+if __name__ == "__main__":
+    try:
+        id = '1k872-nrq8jjICyGyYTNeWbhHBBFaU6Yn'
+        store = file.Storage('token.json')
+        creds = store.get()
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+            creds = tools.run_flow(flow, store)
+        service = build('drive', 'v3', http=creds.authorize(Http()))
+    except Exception:
+        sys.exit("Please check credentials file again")
+       
+verify_FolderID(id)
+    
